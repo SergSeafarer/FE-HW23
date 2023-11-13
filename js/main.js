4ebde7be0a50a096c04f5f261f1f624f46f4293c
Sergii <seregaonma@gmail.com>
2023-11-13 11:40:19 +0200

    create input method for class SuperMath

--- js/main.js
+++ js/main.js
@@ -32,4 +32,61 @@ class SuperMath {
       this.input();
     } 
   }
+
+  input() {
+    const x = prompt('Enter first value?');
+    if(x === null) {
+      return alert('Sorry, but you didnt enter a value');
+    }
+    if(x === '' || x.trim() === '') {
+      return alert('Sorry, but you didnt enter a value');
+    }
+    if(isNaN(x)) {
+      return alert('Sorry, but entered value is not a number');
+    }
+
+    const y = prompt('Enter second value?');
+    if(y === null) {
+      return alert('Sorry, but you didnt enter a value');
+    }
+    if(y === '' || y.trim() === '') {
+      return alert('Sorry, but you didnt enter a value');
+    }
+    if(isNaN(y)) {
+      return alert('Sorry, but entered value is not a number');
+    }
+
+    let znak = prompt('Enter action: (+, -, *, /, %) ?');
+    if(znak !== '+' && znak !== '-' && znak !== '*' && znak !== '/' && znak !== '%') {
+      alert('Choose correct action');
+      znak = prompt('Enter action: (+, -, *, /, %) ?');
+    }
+    switch(znak) {
+      case '/':
+        if(Number(y) !== 0) {
+          alert('Result: ' + (Number(x) / Number(y)));
+          break;
+        } else {
+          alert('Its not allowed devide by zero!!!');
+          break;
+        }
+      case '*':
+        alert('Result: ' + (Number(x) * Number(y)));
+        break;
+      case '+':
+        alert('Result: ' + (Number(x) + Number(y)));
+        break;
+      case '-':
+        alert('Result: ' + (Number(x) - Number(y)));
+        break;
+      case '%':
+        if(Number(y) !== 0) {
+          alert('Result: ' + (Number(x) % Number(y)));
+          break;
+        } else {
+          alert('Its not allowed devide by zero!!!');
+          break;
+        }
+      }
+  }
 }
